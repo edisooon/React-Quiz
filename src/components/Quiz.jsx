e8009927b29891b168bc79763bae5476b947e1ed
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-06-28 16:24:48 -0500

    render the active question in Quiz and implement the answer selection func

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-  const [currentActiveQuestion, setCurrentActiveQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Questions</p>;
+  let activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(answer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, answer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
